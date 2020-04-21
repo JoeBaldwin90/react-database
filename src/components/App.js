64cc6c70a01952db3a93ebf25abe51c51ed522ee
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-04-21 17:41:26 +0100

    Global styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "../css/App.scss";
+import GlobalStyles from "../theme/global-styles";
 import Main from "./Main";
 import Header from "./Header";
 
 const App = () => (
-  <div className='App'>
+  <Fragment>
+    <GlobalStyles dark />
     <Header />
     <Main />
-  </div>
+  </Fragment>
 );
 
 export default App;
---
+++ src/theme/global-styles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300,400,500&display=swap'); // Import fonts
+    font-family: 'IBM Plex Mono', monospace;
+    color: ${(props) => (props.dark ? "mediumseagreen" : "black")};
+    background-color: ${(props) => (props.dark ? "#222222" : "#FFFFFF")};
+  }
+`;
+
+export default GlobalStyles;
